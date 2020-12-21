f1f3b95c540b34b73ffda9a4272ddec5829a2062
Danny Steinbrecher <46423967+darthkali@users.noreply.github.com>
2020-12-21 08:59:07 +0100

    deleted .idea

---
+++ cmake-build-uno/CMakeFiles/Z_DUMMY_TARGET.dir/DependInfo.cmake
@@ -0,0 +1,73 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "P:/LoRa/patenpflanze/.pio/libdeps/uno/ArduinoFake/src/arduino/noniso.c" "P:/LoRa/patenpflanze/cmake-build-uno/CMakeFiles/Z_DUMMY_TARGET.dir/.pio/libdeps/uno/ArduinoFake/src/arduino/noniso.c.obj"
+  "P:/LoRa/patenpflanze/lib/IBM_LMIC_framework/src/aes/lmic.c" "P:/LoRa/patenpflanze/cmake-build-uno/CMakeFiles/Z_DUMMY_TARGET.dir/lib/IBM_LMIC_framework/src/aes/lmic.c.obj"
+  "P:/LoRa/patenpflanze/lib/IBM_LMIC_framework/src/aes/other.c" "P:/LoRa/patenpflanze/cmake-build-uno/CMakeFiles/Z_DUMMY_TARGET.dir/lib/IBM_LMIC_framework/src/aes/other.c.obj"
+  "P:/LoRa/patenpflanze/lib/IBM_LMIC_framework/src/lmic/lmic.c" "P:/LoRa/patenpflanze/cmake-build-uno/CMakeFiles/Z_DUMMY_TARGET.dir/lib/IBM_LMIC_framework/src/lmic/lmic.c.obj"
+  "P:/LoRa/patenpflanze/lib/IBM_LMIC_framework/src/lmic/oslmic.c" "P:/LoRa/patenpflanze/cmake-build-uno/CMakeFiles/Z_DUMMY_TARGET.dir/lib/IBM_LMIC_framework/src/lmic/oslmic.c.obj"
+  "P:/LoRa/patenpflanze/lib/IBM_LMIC_framework/src/lmic/radio.c" "P:/LoRa/patenpflanze/cmake-build-uno/CMakeFiles/Z_DUMMY_TARGET.dir/lib/IBM_LMIC_framework/src/lmic/radio.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "MSVC")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../include"
+  "../src"
+  "../lib/IBM_LMIC_framework/src"
+  "../lib/Ticker"
+  "../.pio/libdeps/uno/Adafruit BMP085 Library"
+  "../.pio/libdeps/uno/ArduinoFake/src"
+  "C:/Users/dannysteinbrecher/.platformio/packages/framework-arduino-avr/cores/arduino"
+  "C:/Users/dannysteinbrecher/.platformio/packages/framework-arduino-avr/variants/standard"
+  "C:/Users/dannysteinbrecher/.platformio/packages/toolchain-atmelavr/lib/gcc/avr/5.4.0/include"
+  "C:/Users/dannysteinbrecher/.platformio/packages/toolchain-atmelavr/lib/gcc/avr/5.4.0/include-fixed"
+  "C:/Users/dannysteinbrecher/.platformio/packages/toolchain-atmelavr/avr/include"
+  "C:/Users/dannysteinbrecher/.platformio/packages/tool-unity"
+  )
+set(CMAKE_DEPENDS_CHECK_CXX
+  "P:/LoRa/patenpflanze/.pio/libdeps/uno/Adafruit BMP085 Library/Adafruit_BMP085.cpp" "P:/LoRa/patenpflanze/cmake-build-uno/CMakeFiles/Z_DUMMY_TARGET.dir/.pio/libdeps/uno/Adafruit_BMP085_Library/Adafruit_BMP085.cpp.obj"
+  "P:/LoRa/patenpflanze/.pio/libdeps/uno/ArduinoFake/examples/mock-injection/src/MyService.cpp" "P:/LoRa/patenpflanze/cmake-build-uno/CMakeFiles/Z_DUMMY_TARGET.dir/.pio/libdeps/uno/ArduinoFake/examples/mock-injection/src/MyService.cpp.obj"
+  "P:/LoRa/patenpflanze/.pio/libdeps/uno/ArduinoFake/examples/mock-injection/test/test_my_service.cpp" "P:/LoRa/patenpflanze/cmake-build-uno/CMakeFiles/Z_DUMMY_TARGET.dir/.pio/libdeps/uno/ArduinoFake/examples/mock-injection/test/test_my_service.cpp.obj"
+  "P:/LoRa/patenpflanze/.pio/libdeps/uno/ArduinoFake/examples/wiring-blink/src/main.cpp" "P:/LoRa/patenpflanze/cmake-build-uno/CMakeFiles/Z_DUMMY_TARGET.dir/.pio/libdeps/uno/ArduinoFake/examples/wiring-blink/src/main.cpp.obj"
+  "P:/LoRa/patenpflanze/.pio/libdeps/uno/ArduinoFake/examples/wiring-blink/test/test_main.cpp" "P:/LoRa/patenpflanze/cmake-build-uno/CMakeFiles/Z_DUMMY_TARGET.dir/.pio/libdeps/uno/ArduinoFake/examples/wiring-blink/test/test_main.cpp.obj"
+  "P:/LoRa/patenpflanze/.pio/libdeps/uno/ArduinoFake/src/ArduinoFake.cpp" "P:/LoRa/patenpflanze/cmake-build-uno/CMakeFiles/Z_DUMMY_TARGET.dir/.pio/libdeps/uno/ArduinoFake/src/ArduinoFake.cpp.obj"
+  "P:/LoRa/patenpflanze/.pio/libdeps/uno/ArduinoFake/src/ClientFake.cpp" "P:/LoRa/patenpflanze/cmake-build-uno/CMakeFiles/Z_DUMMY_TARGET.dir/.pio/libdeps/uno/ArduinoFake/src/ClientFake.cpp.obj"
+  "P:/LoRa/patenpflanze/.pio/libdeps/uno/ArduinoFake/src/FunctionFake.cpp" "P:/LoRa/patenpflanze/cmake-build-uno/CMakeFiles/Z_DUMMY_TARGET.dir/.pio/libdeps/uno/ArduinoFake/src/FunctionFake.cpp.obj"
+  "P:/LoRa/patenpflanze/.pio/libdeps/uno/ArduinoFake/src/PrintFake.cpp" "P:/LoRa/patenpflanze/cmake-build-uno/CMakeFiles/Z_DUMMY_TARGET.dir/.pio/libdeps/uno/ArduinoFake/src/PrintFake.cpp.obj"
+  "P:/LoRa/patenpflanze/.pio/libdeps/uno/ArduinoFake/src/SerialFake.cpp" "P:/LoRa/patenpflanze/cmake-build-uno/CMakeFiles/Z_DUMMY_TARGET.dir/.pio/libdeps/uno/ArduinoFake/src/SerialFake.cpp.obj"
+  "P:/LoRa/patenpflanze/.pio/libdeps/uno/ArduinoFake/src/StreamFake.cpp" "P:/LoRa/patenpflanze/cmake-build-uno/CMakeFiles/Z_DUMMY_TARGET.dir/.pio/libdeps/uno/ArduinoFake/src/StreamFake.cpp.obj"
+  "P:/LoRa/patenpflanze/.pio/libdeps/uno/ArduinoFake/src/arduino/IPAddress.cpp" "P:/LoRa/patenpflanze/cmake-build-uno/CMakeFiles/Z_DUMMY_TARGET.dir/.pio/libdeps/uno/ArduinoFake/src/arduino/IPAddress.cpp.obj"
+  "P:/LoRa/patenpflanze/.pio/libdeps/uno/ArduinoFake/src/arduino/WString.cpp" "P:/LoRa/patenpflanze/cmake-build-uno/CMakeFiles/Z_DUMMY_TARGET.dir/.pio/libdeps/uno/ArduinoFake/src/arduino/WString.cpp.obj"
+  "P:/LoRa/patenpflanze/lib/IBM_LMIC_framework/src/aes/ideetron/AES-128_V10.cpp" "P:/LoRa/patenpflanze/cmake-build-uno/CMakeFiles/Z_DUMMY_TARGET.dir/lib/IBM_LMIC_framework/src/aes/ideetron/AES-128_V10.cpp.obj"
+  "P:/LoRa/patenpflanze/lib/IBM_LMIC_framework/src/hal/hal.cpp" "P:/LoRa/patenpflanze/cmake-build-uno/CMakeFiles/Z_DUMMY_TARGET.dir/lib/IBM_LMIC_framework/src/hal/hal.cpp.obj"
+  "P:/LoRa/patenpflanze/lib/Ticker/Ticker.cpp" "P:/LoRa/patenpflanze/cmake-build-uno/CMakeFiles/Z_DUMMY_TARGET.dir/lib/Ticker/Ticker.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "MSVC")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  "../src"
+  "../lib/IBM_LMIC_framework/src"
+  "../lib/Ticker"
+  "../.pio/libdeps/uno/Adafruit BMP085 Library"
+  "../.pio/libdeps/uno/ArduinoFake/src"
+  "C:/Users/dannysteinbrecher/.platformio/packages/framework-arduino-avr/cores/arduino"
+  "C:/Users/dannysteinbrecher/.platformio/packages/framework-arduino-avr/variants/standard"
+  "C:/Users/dannysteinbrecher/.platformio/packages/toolchain-atmelavr/lib/gcc/avr/5.4.0/include"
+  "C:/Users/dannysteinbrecher/.platformio/packages/toolchain-atmelavr/lib/gcc/avr/5.4.0/include-fixed"
+  "C:/Users/dannysteinbrecher/.platformio/packages/toolchain-atmelavr/avr/include"
+  "C:/Users/dannysteinbrecher/.platformio/packages/tool-unity"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-uno/CMakeFiles/Z_DUMMY_TARGET.dir/cmake_clean.cmake
@@ -0,0 +1,32 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Z_DUMMY_TARGET.dir/.pio/libdeps/uno/Adafruit_BMP085_Library/Adafruit_BMP085.cpp.obj"
+  "CMakeFiles/Z_DUMMY_TARGET.dir/.pio/libdeps/uno/ArduinoFake/examples/mock-injection/src/MyService.cpp.obj"
+  "CMakeFiles/Z_DUMMY_TARGET.dir/.pio/libdeps/uno/ArduinoFake/examples/mock-injection/test/test_my_service.cpp.obj"
+  "CMakeFiles/Z_DUMMY_TARGET.dir/.pio/libdeps/uno/ArduinoFake/examples/wiring-blink/src/main.cpp.obj"
+  "CMakeFiles/Z_DUMMY_TARGET.dir/.pio/libdeps/uno/ArduinoFake/examples/wiring-blink/test/test_main.cpp.obj"
+  "CMakeFiles/Z_DUMMY_TARGET.dir/.pio/libdeps/uno/ArduinoFake/src/ArduinoFake.cpp.obj"
+  "CMakeFiles/Z_DUMMY_TARGET.dir/.pio/libdeps/uno/ArduinoFake/src/ClientFake.cpp.obj"
+  "CMakeFiles/Z_DUMMY_TARGET.dir/.pio/libdeps/uno/ArduinoFake/src/FunctionFake.cpp.obj"
+  "CMakeFiles/Z_DUMMY_TARGET.dir/.pio/libdeps/uno/ArduinoFake/src/PrintFake.cpp.obj"
+  "CMakeFiles/Z_DUMMY_TARGET.dir/.pio/libdeps/uno/ArduinoFake/src/SerialFake.cpp.obj"
+  "CMakeFiles/Z_DUMMY_TARGET.dir/.pio/libdeps/uno/ArduinoFake/src/StreamFake.cpp.obj"
+  "CMakeFiles/Z_DUMMY_TARGET.dir/.pio/libdeps/uno/ArduinoFake/src/arduino/IPAddress.cpp.obj"
+  "CMakeFiles/Z_DUMMY_TARGET.dir/.pio/libdeps/uno/ArduinoFake/src/arduino/WString.cpp.obj"
+  "CMakeFiles/Z_DUMMY_TARGET.dir/.pio/libdeps/uno/ArduinoFake/src/arduino/noniso.c.obj"
+  "CMakeFiles/Z_DUMMY_TARGET.dir/lib/IBM_LMIC_framework/src/aes/ideetron/AES-128_V10.cpp.obj"
+  "CMakeFiles/Z_DUMMY_TARGET.dir/lib/IBM_LMIC_framework/src/aes/lmic.c.obj"
+  "CMakeFiles/Z_DUMMY_TARGET.dir/lib/IBM_LMIC_framework/src/aes/other.c.obj"
+  "CMakeFiles/Z_DUMMY_TARGET.dir/lib/IBM_LMIC_framework/src/hal/hal.cpp.obj"
+  "CMakeFiles/Z_DUMMY_TARGET.dir/lib/IBM_LMIC_framework/src/lmic/lmic.c.obj"
+  "CMakeFiles/Z_DUMMY_TARGET.dir/lib/IBM_LMIC_framework/src/lmic/oslmic.c.obj"
+  "CMakeFiles/Z_DUMMY_TARGET.dir/lib/IBM_LMIC_framework/src/lmic/radio.c.obj"
+  "CMakeFiles/Z_DUMMY_TARGET.dir/lib/Ticker/Ticker.cpp.obj"
+  "Z_DUMMY_TARGET"
+  "Z_DUMMY_TARGET.manifest"
+  "Z_DUMMY_TARGET.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/Z_DUMMY_TARGET.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
